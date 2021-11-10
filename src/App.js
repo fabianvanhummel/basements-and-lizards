d33e8f0c395e801de9c961609981b2167f7b05d6
Pim <pimvschayik@live.nl>
2021-11-10 19:54:02 +0000

    Added some styling to the App
    - Went to this page: https://merakiui.com/#main
    - Looked at the cards and kinda liked the first one
    - Copied the code and made changes to make it fit
    - Removed old redundant code from index.css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import { useState } from 'react'
-import data from './json/maze.json' // book
+import { useState } from "react";
+import data from "./json/maze.json"; // book
 
 function App() {
-  const [location, setLocation] = useState(data['start-location']) // state
+  const [location, setLocation] = useState(data["start-location"]); // state
 
   return (
-    <div className="svg-background min-h-screen">
-      <main>
-        <h1>{ data.locations[location].name }</h1>
-        <p>{ data.locations[location].description }</p>
+    <main className="p-4 svg-background min-h-screen">
+      <div className="max-w-2xl px-8 py-4 mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <p className="text-sm font-light text-gray-600 dark:text-gray-400">
+          The party is at:
+        </p>
 
-        { data.locations[location].directions.map((direction, index) => (
-          <div key={index}>
-            <button
-              onClick={() => setLocation(direction)} // action
-            >
-              { direction }
-            </button>
-          </div>
-        ))}
-      </main>
-    </div>
+        <div className="mt-2">
+          <h1 className="text-2xl font-bold text-gray-700 dark:text-white">
+            {data.locations[location].name}
+          </h1>
+          <p className="mt-2 text-gray-600 dark:text-gray-300">
+            {data.locations[location].description}
+          </p>
+        </div>
+
+        <div className="mt-4">
+          <p className="text-sm font-light text-gray-600 dark:text-gray-400">
+            Move to:
+          </p>
+
+          {data.locations[location].directions.map((direction, index) => (
+            <div key={index} className="flex items-center mt-2">
+              <button
+                onClick={() => setLocation(direction)} // action
+                className="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-200 transform bg-gray-600 rounded cursor-pointer hover:bg-gray-500"
+              >
+                {data.locations[direction].name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
   );
 }
 
