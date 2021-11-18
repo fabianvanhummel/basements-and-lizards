40a4796bbee7f7ee01c18700a9630f3e4e381d9d
JasperH93 <jasperhermsen@hotmail.com>
2021-11-18 13:09:55 +0100

    handleTravelAction en listedPaths gemaakt
    Hoeveelheid logica in return statement verminderd

--- src/App.js
+++ src/App.js
@@ -5,6 +5,44 @@ function App() {
   const [location, setLocation] = useState(data["start-location"]); // state
   const [events, execEvent] = useState(data["events"]); // state
 
+  const listedPaths = data.locations[location].paths.filter(
+    path => {
+      if (data.locations[path].requirements){
+        let reqMet = true
+        data.locations[path].requirements.forEach( (requirement) => {
+          if (!(events[requirement].value)) {
+            reqMet = false
+          }
+        })
+        return reqMet
+      }
+      else {
+        return true
+      }
+    } // Checks paths for requirements
+  )
+
+  const handleTravelAction = (path) => {
+    execEvent({
+      ...events,
+      message: null
+    }); // Clears eventMessages
+    if (data.locations[path].events) {
+      data.locations[path].events.forEach((event) => {
+        if (events[event].value !== events[event].eventValue) {
+          const updatedEvent = events[event]
+          updatedEvent.value = events[event].eventValue // Assign new values
+          execEvent({
+            ...events,
+            [event]: updatedEvent,
+            message: events[event].message
+          }) // Update event State
+        }
+      })
+    }; // Handles Events
+    setLocation(path); // Updates Location
+  }
+
   return (
     <main className="p-4 svg-background min-h-screen">
       <div className="max-w-2xl px-8 py-4 mx-auto bg-white rounded-lg shadow-md dark:bg-gray-800">
@@ -29,44 +67,10 @@ function App() {
             Move to:
           </p>
 
-          {data.locations[location].paths.filter(
-              path => {
-                if (data.locations[path].requirements){
-                  let reqMet = true
-                  data.locations[path].requirements.forEach( (requirement) => {
-                    if (!(events[requirement].value)) {
-                      reqMet = false
-                    }
-                  })
-                  return reqMet
-                }
-                else {
-                  return true
-                }
-              } // Checkt of de paths aan alle requirements voldoen
-            ).map((path, index) => (
+          {listedPaths.map((path, index) => (
             <div key={index} className="flex items-center mt-2">
               <button
-                onClick={() => {
-                  execEvent({
-                    ...events,
-                    message: null
-                  }); // Clears eventMessages
-                  if (data.locations[path].events) {
-                    data.locations[path].events.forEach((event) => {
-                      if (events[event].value !== events[event].eventValue) {
-                        const updatedEvent = events[event]
-                        updatedEvent.value = events[event].eventValue // Assign new values
-                        execEvent({
-                          ...events,
-                          [event]: updatedEvent,
-                          message: events[event].message
-                        }) // Update event State
-                      }
-                    })
-                  }; // Handles Events
-                  setLocation(path); // Updates Location
-                }} // action
+                onClick={() => {handleTravelAction(path)}} // Travel Action
                 className="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-200 transform bg-gray-600 rounded cursor-pointer hover:bg-gray-500"
               >
                 {data.locations[path].name}
