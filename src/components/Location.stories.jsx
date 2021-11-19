81fba00a7ace00c7085e049ba0a8cb168a5b85bf
JasperH93 <jasperhermsen@hotmail.com>
2021-11-19 13:25:53 +0100

    location primary story heeft nu reqMet als faker boolean

--- src/components/Location.stories.jsx
+++ src/components/Location.stories.jsx
@@ -18,7 +18,7 @@ Primary.args = {
     message: Faker.lorem.sentences(),
   })),
   paths: Array(3).fill().map(() => ({
-    reqMet: true,
+    reqMet: Faker.datatype.boolean(),
     to: Faker.address.city(),
     description: Faker.lorem.sentences(),
   })),
