ca4e6a40f85b0df8de7611e954e453445de62b63
JasperH93 <jasperhermsen@hotmail.com>
2021-11-19 10:37:06 +0100

    Path als component opgenomen

---
+++ src/components/Path.jsx
@@ -0,0 +1,26 @@
+
+export const Path = ({to,description,requirements,setLocation}) => (
+  <div class="max-w-2xl px-8 py-4 mx-auto bg-pink-50 rounded-lg shadow-md dark:bg-gray-800">
+    <div class="flex items-center justify-between">
+      <h1 class="text-xl font-bold text-gray-700 dark:text-white">
+        {to}
+      </h1>
+      <div class="px-3 py-1 text-sm font-bold text-gray-100 bg-pink-600 rounded">
+        Path
+      </div>
+    </div>
+
+    <p class="mt-2 text-gray-600 dark:text-gray-300">
+      {description}
+    </p>
+
+    <button
+      onClick={() => setLocation(to)}
+      class="mt-2 px-4 py-2 font-medium tracking-wide text-white transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-80"
+    >
+      Follow this path to { to }
+    </button>
+  </div>
+);
+
+export default Path;
