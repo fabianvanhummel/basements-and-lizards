77cb3b87ee1f9ffb90b8ed3c649df389d6b81baf
JasperH93 <jasperhermsen@hotmail.com>
2021-11-21 11:20:09 +0100

    sortedPaths weggehaald
    de paths worden nu niet meer gesorteerd in de location component

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -2,27 +2,6 @@ import { Event } from "./Event";
 import { Path } from "./Path";
 
 export const Location = ({ name, description, events, paths, setLocation, addEvent }) => {
-  const sortedPaths = 
-    paths && paths.sort(
-      (a,b) => {
-        if(a.reqMet === b.reqMet){
-          const NameToA = a.name ? a.name : a.to
-          const NameToB = b.name ? b.name : b.to
-          const nameA = NameToA.toUpperCase(); // ignore upper and lowercase
-          const nameB = NameToB.toUpperCase(); // ignore upper and lowercase
-          if (nameA < nameB) {
-            return -1;
-          }
-          if (nameA > nameB) {
-            return 1;
-          }
-        }
-        else if (a.reqMet) {
-          return -1
-        }
-        return 1
-      } // Sort locations on met requirements and alfabetic
-    );
   return (
     <div class="max-w-2xl px-8 py-4 mx-auto bg-green-50 rounded-lg shadow-md dark:bg-gray-800">
       <div class="flex items-center justify-between">
@@ -49,13 +28,13 @@ export const Location = ({ name, description, events, paths, setLocation, addEve
         </div>
       )}
 
-      {sortedPaths && (
+      {paths && (
         <div class="mt-4">
           <span class="text-sm font-light text-gray-600 dark:text-gray-400">
             Paths
           </span>
           <ul>
-            {sortedPaths.map((path, index) => (
+            {paths.map((path, index) => (
               <li key={index} class="mt-2">
                 <Path {...path} setLocation={setLocation} />
               </li>
