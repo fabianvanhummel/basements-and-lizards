b881350fd92c1954d3bc2b3575b9cc2ac36801b9
JasperH93 <jasperhermsen@hotmail.com>
2021-11-21 11:16:57 +0100

    Button disabled op reqMet

--- src/components/Path.jsx
+++ src/components/Path.jsx
@@ -15,7 +15,8 @@ export const Path = ({ reqMet, to, name, description, setLocation}) => (
     </p>
 
     <button
-      onClick={() => {if (reqMet) {setLocation(to)}}}
+      disabled={!reqMet}
+      onClick={() => {setLocation(to)}}
       class="mt-2 px-4 py-2 font-medium tracking-wide text-white transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-80"
     >
       {reqMet ? "Follow this path to " + to : <strike> {"Follow this path to " + to} </strike>}
