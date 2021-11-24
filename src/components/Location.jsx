f14d7489e4a6006c8972260d4cf316146d0698f8
Pim <pimvschayik@live.nl>
2021-11-24 16:44:46 +0000

    use className instead of class

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -3,24 +3,24 @@ import { Path } from "./Path";
 
 export const Location = ({ name, description, events, paths, setLocation, addEvent }) => {
   return (
-    <div class="max-w-2xl px-8 py-4 mx-auto bg-green-50 rounded-lg shadow-md dark:bg-gray-800">
-      <div class="flex items-center justify-between">
-        <h1 class="text-2xl font-bold text-gray-700 dark:text-white">{name}</h1>
-        <div class="px-3 py-1 text-sm font-bold text-gray-100 bg-green-600 rounded">
+    <div className="max-w-2xl px-8 py-4 mx-auto bg-green-50 rounded-lg shadow-md dark:bg-gray-800">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold text-gray-700 dark:text-white">{name}</h1>
+        <div className="px-3 py-1 text-sm font-bold text-gray-100 bg-green-600 rounded">
           Location
         </div>
       </div>
 
-      <p class="mt-2 text-gray-600 dark:text-gray-300">{description}</p>
+      <p className="mt-2 text-gray-600 dark:text-gray-300">{description}</p>
 
       {events && (
-        <div class="mt-4">
-          <span class="text-sm font-light text-gray-600 dark:text-gray-400">
+        <div className="mt-4">
+          <span className="text-sm font-light text-gray-600 dark:text-gray-400">
             Events
           </span>
           <ul>
             {events.map((event, index) => (
-              <li key={index} class="mt-2">
+              <li key={index} className="mt-2">
                 <Event {...event} addEvent={addEvent} />
               </li>
             ))}
@@ -29,13 +29,13 @@ export const Location = ({ name, description, events, paths, setLocation, addEve
       )}
 
       {paths && (
-        <div class="mt-4">
-          <span class="text-sm font-light text-gray-600 dark:text-gray-400">
+        <div className="mt-4">
+          <span className="text-sm font-light text-gray-600 dark:text-gray-400">
             Paths
           </span>
           <ul>
             {paths.map((path, index) => (
-              <li key={index} class="mt-2">
+              <li key={index} className="mt-2">
                 <Path {...path} setLocation={setLocation} />
               </li>
             ))}
