d7648dac289bb7a55a218193ff4ebbacd303c9c8
JasperH93 <jasperhermsen@hotmail.com>
2021-11-25 14:13:15 +0100

    Event gebruikt nu name en description

--- src/components/Event.stories.jsx
+++ src/components/Event.stories.jsx
@@ -11,12 +11,16 @@ const Template = (args) => <Event {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
+  name: Faker.lorem.word(),
+  description: Faker.lorem.words(),
   message: Faker.lorem.sentences(),
   addEvent: () => {},
 };
 
 export const hasHappened = Template.bind({});
 hasHappened.args = {
+  name: Faker.lorem.word(),
+  description: Faker.lorem.words(),
   hasHappened: true,
   message: Faker.lorem.sentences(),
   addEvent: () => {},
