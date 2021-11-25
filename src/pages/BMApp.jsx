96b7e4b299e6bdb68f7d9202a4bd26f683f89c5c
Pim <pimvschayik@live.nl>
2021-11-25 16:32:39 +0000

    Added navbar to BMApp to navigate between tabs

--- src/pages/BMApp.jsx
+++ src/pages/BMApp.jsx
@@ -55,6 +55,64 @@ export const BMApp = ({ book }) => {
 
   return (
     <BrowserRouter>
+      <nav className="bg-white shadow dark:bg-gray-800">
+        <div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1
+                className="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl"
+                to="/"
+              >
+                Basements & Lizards
+              </h1>
+            </div>
+
+            <div className="flex md:hidden">
+              <button
+                type="button"
+                className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                aria-label="toggle menu"
+              >
+                <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+                  <path
+                    fillRule="evenodd"
+                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div className="items-center md:flex">
+            <div className="flex flex-col md:flex-row md:mx-6">
+              <Link
+                className="my-1 text-sm font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 md:mx-4 md:my-0"
+                to="/"
+              >
+                Home
+              </Link>
+              <Link
+                className="my-1 text-sm font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 md:mx-4 md:my-0"
+                to="/location"
+              >
+                Location
+              </Link>
+              <Link
+                className="my-1 text-sm font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 md:mx-4 md:my-0"
+                to="/inventory"
+              >
+                Inventory
+              </Link>
+              <Link
+                className="my-1 text-sm font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 md:mx-4 md:my-0"
+                to="/history"
+              >
+                History
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
 
       <Routes>
         <Route path="/" element={<div>StartTabPlaceholder</div>} />
