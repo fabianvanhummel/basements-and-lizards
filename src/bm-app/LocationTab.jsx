035d9ec65c6c263f343c6b4c451c30869c0fd207
Pim <pimvschayik@live.nl>
2021-11-29 19:07:01 +0000

    bring back location UI

--- src/bm-app/LocationTab.jsx
+++ src/bm-app/LocationTab.jsx
@@ -1,3 +1,29 @@
 import React from "react";
+import { Location } from "../components/Location";
 
-export const LocationTab = () => <div>LocationTabPlaceholder</div>;
+export const LocationTab = ({
+  name,
+  description,
+  events,
+  items,
+  paths,
+  setLocation,
+  addEvent,
+  addItem,
+}) => (
+  <div className="p-4">
+    <p className="mb-2 text-sm font-light text-gray-600 dark:text-gray-400 text-center text-3xl font-sans">
+      You are at
+    </p>
+    <Location
+      name={name}
+      description={description}
+      events={events}
+      items={items}
+      paths={paths}
+      setLocation={setLocation}
+      addEvent={addEvent}
+      addItem={addItem}
+    />
+  </div>
+);
