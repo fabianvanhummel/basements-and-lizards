9af91ea35b943612108484e7c6e763d7ac81f6e1
JasperH93 <jasperhermsen@hotmail.com>
2021-12-04 19:23:02 +0100

    collapse menu gemaakt met tailwind classes

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,6 +1,7 @@
 import { Event } from "./Event";
 import { Item } from "./Item";
 import { Path } from "./Path";
+import { useState } from "react";
 
 export const Location = ({
   name,
@@ -12,6 +13,7 @@ export const Location = ({
   addEvent,
   addItem,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const availablePaths =
     paths &&
     paths.filter((path) => {
@@ -84,28 +86,47 @@ export const Location = ({
             ""
           )}
           {blockedPaths.length ? (
-            <div className="tab mt-4">
-              <input
-                className="absolute opacity-0 "
-                id="tab-multi-one"
-                type="checkbox"
-                name="tabs"
-              ></input>
-              <label
-                className="block p-5 cursor-pointer"
-                htmlFor="tab-multi-one"
+            <div className="border rounded shadow-sm">
+              <button
+                type="button"
+                aria-label="Open item"
+                title="Open item"
+                className="flex items-center justify-between w-full p-4 focus:outline-none"
+                onClick={() => setIsOpen(!isOpen)}
               >
-                Blocked Paths
-              </label>
-              <div className="tab-content overflow-hidden">
-                <ul>
-                  {blockedPaths.map((path, index) => (
-                    <li key={index} className="mt-2">
-                      <Path {...path} setLocation={setLocation} />
-                    </li>
-                  ))}
-                </ul>
-              </div>
+                <p className="text-lg font-medium">Blocked Paths</p>
+                <div className="flex items-center justify-center w-8 h-8 border rounded-full">
+                  <svg
+                    viewBox="0 0 24 24"
+                    className={`w-3 text-gray-600 transition-transform duration-200 ${
+                      isOpen ? "transform rotate-180" : ""
+                    }`}
+                  >
+                    <polyline
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeMiterlimit="10"
+                      points="2,7 12,17 22,7"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
+              </button>
+              {isOpen && (
+                <div className="p-4 pt-0">
+                  {
+                    <ul>
+                      {blockedPaths.map((path, index) => (
+                        <li key={index} className="mt-2">
+                          <Path {...path} setLocation={setLocation} />
+                        </li>
+                      ))}
+                    </ul>
+                  }
+                </div>
+              )}
             </div>
           ) : (
             ""
