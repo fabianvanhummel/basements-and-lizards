9df8366e13b3b321ec5ba0c763868f69e6022b47
JasperH93 <jasperhermsen@hotmail.com>
2021-12-06 14:38:08 +0100

    Fix voor event stories met decorator context

--- src/components/Event.stories.jsx
+++ src/components/Event.stories.jsx
@@ -2,26 +2,35 @@ import React from "react";
 import Faker from "faker";
 import { Event } from "./Event";
 
+import { ContextProvider } from "../bm-app/context";
+
+const eventStory = {
+  "start-location": 1,
+  locations: {
+    1: {},
+  },
+  events: {
+    1: {
+      name: Faker.lorem.word(),
+      description: Faker.lorem.words(),
+      message: Faker.lorem.sentences(),
+    },
+  },
+};
+
 export default {
   title: "Components/Event",
   component: Event,
+  decorators: [
+    (StoryFn) => {
+      return <ContextProvider book={eventStory}>{StoryFn()}</ContextProvider>;
+    },
+  ],
 };
 
 const Template = (args) => <Event {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  name: Faker.lorem.word(),
-  description: Faker.lorem.words(),
-  message: Faker.lorem.sentences(),
-  addEvent: () => {},
-};
-
-export const didHappen = Template.bind({});
-didHappen.args = {
-  name: Faker.lorem.word(),
-  description: Faker.lorem.words(),
-  didHappen: true,
-  message: Faker.lorem.sentences(),
-  addEvent: () => {},
+  id: "1",
 };
