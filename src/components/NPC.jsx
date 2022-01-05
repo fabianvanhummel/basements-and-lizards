2481fbb6517bdf3c27435286402982363bbf097d
JasperH93 <jasperhermsen@hotmail.com>
2022-01-05 11:45:09 +0100

    NPC conversation toegevoegd

--- src/components/NPC.jsx
+++ src/components/NPC.jsx
@@ -1,4 +1,61 @@
-export const NPC = ({ name, description }) => {
+import { useState } from "react";
+
+export const NPC = ({ reqMet, name, description, conversation }) => {
+  const [conversationStarted, setConversationStarted] = useState(false);
+  const [conversationId, setConversationId] = useState("initial");
+
+  const conversationToggle = () => {
+    setConversationStarted(!conversationStarted);
+  };
+
+  const handleConsequences = (consequences) => {
+    consequences.forEach((consequence) => {
+      switch (consequence.type) {
+        case "CONVERSATION":
+          setConversationId(consequence.id);
+          break;
+        default:
+          // Do Nothing
+          break;
+      }
+    });
+  };
+
+  const conversationHandler = () => {
+    return (
+      <div className="max-w-2xl px-8 py-4 mx-auto bg-purple-300 rounded-lg shadow-md dark:bg-gray-800">
+        <span className="text-sm font-light text-gray-600 dark:text-gray-400">
+          {name + " Says:"}
+        </span>
+        <p>{conversation[conversationId].npcLine}</p>
+
+        {conversation[conversationId].responses ? (
+          <div className="mt-4">
+            <span className="text-sm font-light text-gray-600 dark:text-gray-400">
+              How will you respond?
+            </span>
+            <ul>
+              {conversation[conversationId].responses.map((response, index) => (
+                <li key={index} className="mt-2">
+                  <button
+                    onClick={() => {
+                      handleConsequences(response.consequences);
+                    }}
+                    className="disabled:opacity-50 disabled:pointer-events-none disabled:cursor-auto mt-2 px-4 py-2 font-medium tracking-wide text-white transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-80"
+                  >
+                    {response.text}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p className="font-bold">The conversation is now over</p>
+        )}
+      </div>
+    );
+  };
+
   return (
     <div className="max-w-2xl px-8 py-4 mx-auto bg-purple-50 rounded-lg shadow-md dark:bg-gray-800">
       <div className="flex items-center justify-between">
@@ -9,8 +66,34 @@ export const NPC = ({ name, description }) => {
           NPC
         </div>
       </div>
-
       <p className="mt-2 text-gray-600 dark:text-gray-300">{description}</p>
+
+      {!conversationStarted ? (
+        <button
+          disabled={!reqMet}
+          onClick={() => {
+            conversationToggle();
+          }}
+          className="disabled:opacity-50 disabled:pointer-events-none disabled:cursor-auto mt-2 px-4 py-2 font-medium tracking-wide text-white transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-80"
+        >
+          {"Approach " + name}
+        </button>
+      ) : (
+        <div>
+          {conversationHandler()}
+          <div className="text-right">
+            <button
+              onClick={() => {
+                conversationToggle();
+                setConversationId("initial");
+              }}
+              className="disabled:opacity-50 disabled:pointer-events-none disabled:cursor-auto mt-2 px-4 py-2 font-medium tracking-wide text-white transition-colors duration-200 transform bg-indigo-600 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-80"
+            >
+              End conversation
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/NPC.stories.jsx
+++ src/components/NPC.stories.jsx
@@ -11,6 +11,79 @@ const Template = (args) => <NPC {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
+  reqMet: true,
+  name: Faker.name.findName(),
+  description: Faker.lorem.sentences(),
+  conversation: {
+    initial: {
+      npcLine: Faker.lorem.sentence(),
+      responses: [
+        {
+          text: Faker.lorem.sentence(),
+          consequences: [
+            {
+              type: "CONVERSATION",
+              id: 1,
+            },
+          ],
+        },
+      ],
+    },
+    1: {
+      npcLine: Faker.lorem.sentence(),
+      responses: [
+        {
+          text: Faker.lorem.sentence(),
+          consequences: [
+            {
+              type: "CONVERSATION",
+              id: 2,
+            },
+          ],
+        },
+        {
+          text: "Bye!",
+          consequences: [
+            {
+              type: "CONVERSATION",
+              id: "end-conversation",
+            },
+          ],
+        },
+      ],
+    },
+    2: {
+      npcLine: Faker.lorem.sentence(),
+      responses: [
+        {
+          text: Faker.lorem.sentence(),
+          consequences: [
+            {
+              type: "CONVERSATION",
+              id: 1,
+            },
+          ],
+        },
+        {
+          text: "Bye!",
+          consequences: [
+            {
+              type: "CONVERSATION",
+              id: "end-conversation",
+            },
+          ],
+        },
+      ],
+    },
+    "end-conversation": {
+      npcLine: "Ok, Bye!",
+    },
+  },
+};
+
+export const Unavailable = Template.bind({});
+Unavailable.args = {
+  reqMet: false,
   name: Faker.name.findName(),
   description: Faker.lorem.sentences(),
 };
