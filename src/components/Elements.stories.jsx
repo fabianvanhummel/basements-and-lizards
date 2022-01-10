f9a0c0b2b314a012915737bc5e6d9976817b1ceb
Djep0 <jasperhermsen@hotmail.com>
2022-01-10 13:48:27 +0100

    element stories

--- src/components/Elements.stories.jsx
+++ src/components/Elements.stories.jsx
@@ -10,9 +10,8 @@ export default {
 const Template = (args) => <ElementList {...args} />;
 
 export const PrimaryEvents = Template.bind({});
-// Note: this is not what the actual data structure is like.
 PrimaryEvents.args = {
-  elementType: "Events",
+  type: "Events",
   elements: Array(5)
     .fill()
     .map(() => ({
@@ -21,24 +20,58 @@ PrimaryEvents.args = {
       didHappen: Faker.datatype.boolean(),
       message: Faker.lorem.sentences(),
       reqMet: Faker.datatype.boolean(),
-      showBlocked: Faker.datatype.boolean(),
-      addEvent: () => {}
+      addEvent: () => {},
     })),
 };
-/*
-  items: Array(3)
+
+export const PrimaryItems = Template.bind({});
+PrimaryItems.args = {
+  type: "Items",
+  elements: Array(3)
     .fill()
     .map(() => ({
       isPresent: Faker.datatype.boolean(),
       name: Faker.commerce.productName(),
       description: Faker.commerce.productDescription(),
+      reqMet: Faker.datatype.boolean(),
+      addItem: () => {},
     })),
-  npcs: Array(2)
+};
+
+export const PrimaryNpcs = Template.bind({});
+PrimaryNpcs.args = {
+  type: "Npcs",
+  elements: Array(2)
     .fill()
     .map(() => ({
       name: Faker.name.findName(),
       description: Faker.lorem.sentences(),
+      reqMet: Faker.datatype.boolean(),
+      conversation: {
+        initial: {
+          npcLine: null,
+          responses: [],
+        },
+      },
     })),
+};
+
+export const PrimaryPaths = Template.bind({});
+PrimaryPaths.args = {
+  type: "Paths",
+  elements: Array(4)
+  .fill()
+  .map(() => ({
+    reqMet: Faker.datatype.boolean(),
+    toLocationId: Faker.lorem.word(),
+    name: Faker.address.city(),
+    description: Faker.lorem.sentences(),
+    reqMet: Faker.datatype.boolean(),
+    setLocation: () => {},
+  })),
+};
+
+/*
   paths: Array(4)
     .fill()
     .map(() => ({
