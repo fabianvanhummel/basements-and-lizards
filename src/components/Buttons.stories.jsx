b7cd27afb9f8d29f6c6b593fdd45e9c98d3b8863
Djep0 <jasperhermsen@hotmail.com>
2022-01-10 10:02:09 +0100

    button stories

---
+++ src/components/Buttons.stories.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Faker from "faker";
+import { DefaultButton, ButtonToggleBlocked } from "./Buttons";
+
+export default {
+  title: "Components/Buttons",
+  //component: DefaultButton,
+};
+
+const DefaultButtonTemplate = (args) => <DefaultButton {...args} />;
+
+const ButtonToggleBlockedTemplate = (args) => <ButtonToggleBlocked {...args} />;
+
+export const DefaultButtonPrimary = DefaultButtonTemplate.bind({});
+DefaultButtonPrimary.args = {
+  buttonText: Faker.lorem.words(),
+  onClick: () => {},
+  isDisabled: false,
+};
+
+export const ButtonToggleBlockedPrimary = ButtonToggleBlockedTemplate.bind({});
+ButtonToggleBlockedPrimary.args = {
+  toggleShowBlockedState: () => {},
+};
