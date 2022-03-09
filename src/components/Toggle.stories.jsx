cdb5517971ba9dd80cb8b17f15dd152b6cbb736d
Fabian van Hummel <fabianvanhummel@gmail.com>
2022-03-09 11:43:33 +0100

    pims suggestie

--- src/components/Toggle.stories.jsx
+++ src/components/Toggle.stories.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Toggle } from "./Toggle";
+import { useState } from "react";
 
 export default {
   title: "Components/Toggle",
 };
 
-const ToggleTemplate = (args) => <Toggle {...args} />;
-
-export const TogglePrimary = ToggleTemplate.bind({});
-TogglePrimary.args = {
-  toggledArg: true,
+export const TogglePrimary = () => {
+  const [toggled, setToggled] = useState(false);
+  return <Toggle toggled={toggled} toggle={(toggled) => setToggled(toggled)} />;
 };
