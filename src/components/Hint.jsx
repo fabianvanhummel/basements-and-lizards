2c139ddc0702af2b5086d0a1774c9337df6d001f
Djep0 <jasperhermsen@hotmail.com>
2022-10-25 15:12:25 +0200

    Help text leesbaar op mobiel
    Boxje is groter, kleinere text, meer ruimte in boxje gebruikt.

--- src/components/Hint.jsx
+++ src/components/Hint.jsx
@@ -1,14 +1,14 @@
 export const Hint = ({ hintText, toggle }) => {
   return (
-    <div className="absolute m-auto	inset-1/4 bg-orange-200 rounded-lg z-10">
-      <p className="font-serif text-5xl text-green-600 text-center py-4">
+    <div className="absolute inset-x-0 bg-orange-200 rounded-lg">
+      <p className="font-serif text-3xl text-green-600 text-center py-2">
         Hint
       </p>
-      <p className="font-serif text-lg text-green-600 mx-16 overflow-auto max-h-80">
+      <p className="font-serif text-sm text-green-600 mx-2 py-2 overflow-auto max-h-80">
         {hintText}
       </p>
       <button
-        className="absolute rounded-lg bg-green-200 max-w-max p-4 cursor-pointer right-2 bottom-2"
+        className="rounded-lg bg-green-200 p-4 cursor-pointer flex mx-auto"
         onClick={toggle}
       >
         Close
