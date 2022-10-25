e58433608635698a471592a57f8621e0f5fbf080
Pim <pimvschayik@live.nl>
2022-10-25 19:18:27 +0100

    Fix menu
    - `classNames` package maakt het makkelijker om conditional classes toe te voegen.
    - `isMenuOpen` state om bij te houden of het menu open of dicht is.
    - Wat random changes omdat ik een ander template gepakt heb.

--- src/bm-app/Router.jsx
+++ src/bm-app/Router.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import classNames from "classnames";
 import { OverviewTab } from "./OverviewTab";
 import { GameTab } from "./GameTab";
 import { InventoryTab } from "./InventoryTab";
@@ -15,6 +17,8 @@ export const Router = ({
   handleAction,
   backgroundImageUrl,
 }) => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   return (
     <div
       className="fade-in-1s background"
@@ -23,64 +27,97 @@ export const Router = ({
       }}
     >
       <BrowserRouter>
-        <nav className="bg-white shadow dark:bg-gray-800">
+        <nav className="relative bg-white shadow dark:bg-gray-800">
           <div className="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
             <div className="flex items-center justify-between">
               <div>
-                <h1
-                  className="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl"
-                  to="/"
-                >
+                <h1 className="text-2xl font-bold text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300">
                   Basements & Lizards
                 </h1>
               </div>
 
-              <div className="flex md:hidden">
+              <div className="flex lg:hidden">
                 <button
+                  onClick={() => setMenuOpen(!isMenuOpen)}
                   type="button"
                   className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
                   aria-label="toggle menu"
                 >
-                  <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-                    <path
-                      fillRule="evenodd"
-                      d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                    ></path>
-                  </svg>
+                  {!isMenuOpen && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M4 8h16M4 16h16"
+                      />
+                    </svg>
+                  )}
+
+                  {isMenuOpen && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  )}
                 </button>
               </div>
             </div>
 
-            <div className="items-center md:flex">
+            <div
+              className={classNames(
+                "absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center",
+                {
+                  "translate-x-0 opacity-100": isMenuOpen,
+                  "opacity-0 -translate-x-full": !isMenuOpen,
+                },
+              )}
+            >
               <div className="flex flex-col md:flex-row md:mx-6">
                 <Link
-                  className="my-1 text-sm font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 md:mx-4 md:my-0"
+                  className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
                   to="/basements-and-lizards"
                 >
                   Home
                 </Link>
                 <Link
-                  className="my-1 text-sm font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 md:mx-4 md:my-0"
+                  className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
                   to="/basements-and-lizards/game"
                 >
                   Game
                 </Link>
                 <Link
-                  className="my-1 text-sm font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 md:mx-4 md:my-0"
+                  className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
                   to="/basements-and-lizards/inventory"
                 >
                   Inventory
                 </Link>
                 <Link
-                  className="my-1 text-sm font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 md:mx-4 md:my-0"
+                  className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
                   to="/basements-and-lizards/history"
                 >
                   History
                 </Link>
                 {gameState.gameFinished && (
                   <Link
-                    className="my-1 text-sm font-medium text-gray-700 dark:text-gray-200 hover:text-indigo-500 dark:hover:text-indigo-400 md:mx-4 md:my-0"
-                    to="/basements-and-lizards/finish"
+                    className="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+                    to="/basements-and-lizards/history"
                   >
                     Finish
                   </Link>
