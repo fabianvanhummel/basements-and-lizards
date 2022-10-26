4dd57a338a81dc55a41939af06d0a1cab235866f
Pim van Schayik <32970994+Schayik@users.noreply.github.com>
2022-10-26 18:19:24 +0100

    Remove de twee kut-background

--- src/images/backgrounds.js
+++ src/images/backgrounds.js
@@ -1,6 +1,4 @@
 export const backgroundImages = [
   "https://i.ibb.co/121sqp4/14.jpg",
   "https://i.ibb.co/vXrxSc1/wallpapersden-com-minimalist-landscape-painting-1920x1080.jpg",
-  "https://i.ibb.co/VxsbgD5/white-minimalism-01.jpg",
-  "https://i.ibb.co/PxFqrXB/hand-drawn-minimal-background-23-2149006548.webp",
 ]
